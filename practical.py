34a2fe35652d60f0082e173155a20d5e091247dd
Ketan Belekar <ketanbelekar8960@gmail.com>
2026-05-15 10:24:07 +0530

    Add functionality to find greatest and smallest numbers

---
+++ practical.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+greatest = max(a, b, c)
+smallest = min(a, b, c)
+
+print("Greatest number is:", greatest)
+print("Smallest number is:", smallest)
